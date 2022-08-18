c550ac2de0280ecf9cc2850d1a73f32d9e9642ad
Pain Killer <gachungasamuel53@gmail.com>
2022-08-18 20:29:45 +0300

    finished

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,33 +1,6 @@
 import React, { Component } from "react";
-import Modal from "./components/Modal"
+import Modal from "./components/Modal";
 import axios from "axios";
-
-// const todoItems = [
-//   {
-//     id: 1,
-//     title: "Go to Market",
-//     description: "Buy ingredients to prepare dinner",
-//     completed: true,
-//   },
-//   {
-//     id: 2,
-//     title: "Study",
-//     description: "Read Algebra and History textbook for the upcoming test",
-//     completed: false,
-//   },
-//   {
-//     id: 3,
-//     title: "Sammy's books",
-//     description: "Go to library to return Sammy's books",
-//     completed: true,
-//   },
-//   {
-//     id: 4,
-//     title: "Article",
-//     description: "Write article on how to use Django with React",
-//     completed: false,
-//   },
-// ];
 
 class App extends Component {
   constructor(props) {
@@ -50,14 +23,14 @@ class App extends Component {
 
   refreshList = () => {
     axios
-      .get("/api/todod/")
-      .then((res) => this.setState({ todoList: res.data}))
+      .get("/api/todos/")
+      .then((res) => this.setState({ todoList: res.data }))
       .catch((err) => console.log(err));
   };
 
   toggle = () => {
     this.setState({ modal: !this.state.modal });
-  }
+  };
 
   handleSubmit = (item) => {
     this.toggle();
@@ -65,31 +38,29 @@ class App extends Component {
     if (item.id) {
       axios
         .put(`/api/todos/${item.id}/`, item)
-        .then((res) => this.refershList())
+        .then((res) => this.refreshList());
       return;
     }
     axios
       .post("/api/todos/", item)
-      .then((res) => this.refershList());
+      .then((res) => this.refreshList());
   };
 
   handleDelete = (item) => {
     axios
-      .delete(`/pai/todos/${item.id}/`)
-      .then((res) => this.refershList())
-  }
+      .delete(`/api/todos/${item.id}/`)
+      .then((res) => this.refreshList());
+  };
 
   createItem = () => {
-    const item = { title: "", description: "", completed: false}
+    const item = { title: "", description: "", completed: false };
 
-    this.setState({ activateItem: item, modal: !this.state.modal })
-  }
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
 
   editItem = (item) => {
-    this.setState({ activeItem: item, modal: !this.state.modal })
-  }
-
-
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
 
   displayCompleted = (status) => {
     if (status) {
@@ -103,14 +74,14 @@ class App extends Component {
     return (
       <div className="nav nav-tabs">
         <span
-          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
           onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
         >
           Complete
         </span>
         <span
-          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
           onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
         >
           Incomplete
         </span>
@@ -121,7 +92,7 @@ class App extends Component {
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(
-      (item) => item.completed == viewCompleted
+      (item) => item.completed === viewCompleted
     );
 
     return newItems.map((item) => (
@@ -178,7 +149,7 @@ class App extends Component {
           </div>
         </div>
         {this.state.modal ? (
-          <Modal 
+          <Modal
             activeItem={this.state.activeItem}
             toggle={this.toggle}
             onSave={this.handleSubmit}
